ed5b6f461f083c01248a61afa15d51b3b00fb408
Sai Viraj <saivirajthegreat@gmail.com>
2025-05-31 22:37:50 +0530

    commit

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,8 +1,7 @@
 import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
-import { styles } from "../styles";
 
-import { SectionWrapper } from "../hoc";
+
 
 const Contact = () => {
   const formRef = useRef();
@@ -57,8 +56,8 @@ const Contact = () => {
       className="xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden"
     >
       <div className="gsap-form flex-[0.75] bg-black-100 p-8 rounded-2xl">
-        <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Contact.</h3>
+        <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wide">Get in touch</p>
+        <h3 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">Contact.</h3>
 
         <form
           ref={formRef}
@@ -112,4 +111,4 @@ const Contact = () => {
 };
 
 // eslint-disable-next-line react-refresh/only-export-components
-export default SectionWrapper(Contact, "contact");
+export default Contact;
